6313129df0ce19c4cdd2c61f5a1cee4ece22a548
Christopher Griffin <gri15021@byui.edu>
2017-06-25 04:21:06 -0700

    Basic Fluency Completion

--- assets/js/global.js
+++ assets/js/global.js
@@ -8,6 +8,10 @@ function newElement(tag) {return document.createElement(tag);  }
 function newText(text)   {return document.createTextNode(text);}
 function newEvent(id, event, action) {getByID(id).addEventListener(event, action);}
 
+// DESTROY
+function delElement(parent, child) {getByID(parent).removeChild(child);}
+function delEvent(id, event, action) {getByID(id).removeEventListener(event, action);}
+
 // GET ELEMENTS
 function getByID(id)        {return document.getElementById(id);          }
 function getByClass(name)   {return document.getElementsByClassName(name);}
